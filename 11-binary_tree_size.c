421c643f5a66f0200920c27ba4af32e8f33e27e2
Reem-degais <reem.degais.33@gmail.com>
2023-08-02 07:33:11 -0700

    -

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -15,6 +15,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		left = binary_tree_size(tree->left);
 	if (tree->right)
 		right = binary_tree_size(tree->right);
+
 	return (left + right + 1);
 
 }

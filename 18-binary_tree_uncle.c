d1b09e3a089fe0d9a5f9bd839819750f33562e73
Reem-degais <reem.degais.33@gmail.com>
2023-08-02 19:22:55 -0700

    -

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,16 +7,16 @@
  */
 binary_tree_t *search_for_uncle(binary_tree_t *node)
 {
-        binary_tree_t *grand = NULL;
+	binary_tree_t *grand = NULL;
 
-        if (!node || !(node->parent))
-                return (NULL);
-        grand = node->parent;
-        if (grand->left && (grand->left != node))
-                return (grand->left);
-        else if (grand->right && (grand->right != node))
-                return (grand->right);
-        return (NULL);
+	if (!node || !(node->parent))
+		return (NULL);
+	grand = node->parent;
+	if (grand->left && (grand->left != node))
+		return (grand->left);
+	else if (grand->right && (grand->right != node))
+		return (grand->right);
+	return (NULL);
 }
 
 /**
@@ -26,7 +26,7 @@ binary_tree_t *search_for_uncle(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-        if (!node || !(node->parent))
-                return (NULL);
-        return (search_for_uncle(node->parent));
+	if (!node || !(node->parent))
+		return (NULL);
+	return (search_for_uncle(node->parent));
 }
